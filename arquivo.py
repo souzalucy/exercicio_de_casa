6d86e8c09afc48304d241f587fda88f0caf69cff
Lucy <souzalucyg@gmail.com>
2024-04-06 17:25:24 -0300

    aula 2

---
+++ arquivo.py
@@ -0,0 +1,7 @@
+def calc_media():
+    numero1 = int(input("Digite o numero 1: "))
+    numero2 = int(input("Digite o numero 2: "))
+    soma = (numero1 + numero2)/2
+    return soma
+
+print(calc_media())
